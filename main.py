4485c61857b5942400aea6ed25264c1a0e8187e0
iman sharifi <imans494@gmail.com>
2018-12-02 19:01:46 +0330

    module and librari names are here fixes #1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cli import CliHelper
-import os
+import os, sys
+# import sys
 from contextlib import closing
+import itertools
 
 
 class OpenFiles:
@@ -21,11 +23,29 @@ class OpenFiles:
 				for line in fin:
 					yield line
 
-	def has_import(self):
+
+class ModuleOrLibrary(OpenFiles):
+
+	def import_getter(self):
 		unique_lines = set()
 		for line in self.open_files():
-			if 'import ' in line:
-				unique_lines.add(line.replace('\n', '').replace('\t', ''))
-			else:
-				print("no imported library found")
-		print(unique_lines)
+			try:
+				if 'import ' in line:
+					unique_lines.add(line.replace('\n', '').replace('\t', ''))
+			except:
+				sys.exit('there is no library or module imported in this directory')
+		return unique_lines
+
+	def module_and_lib_set(self):
+		module_and_lib_names = []
+
+		for every_import in self.import_getter():
+			if every_import.startswith('import'):
+				names_list = every_import.split('import ')[1].split(', ')
+				module_and_lib_names.append(names_list)
+			elif every_import.startswith('from'):
+				names_list = [every_import.split('from ')[1].split()[0]]
+				module_and_lib_names.append(names_list)
+		convert_to_flat = list(itertools.chain.from_iterable(module_and_lib_names))
+		module_and_lib_names = set(convert_to_flat)
+		return module_and_lib_names
